1335c7f46ec8237de4343f2c71772fecd9b4f65d
RiccardoNestola <riccardinux@gmail.com>
2023-02-09 15:54:28 +0100

    create model and controller

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,13 @@
 <header>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">BOOKS</span>
-        </div>
-      </nav>
+    <div class="container">
+        
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">BOOKS</span>
+            </div>
+        </nav>
+
+    </div>
+    
 </header>
 
