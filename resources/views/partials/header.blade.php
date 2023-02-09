809547d8398717c02d83525865cd7024184d3fbb
RiccardoNestola <riccardinux@gmail.com>
2023-02-09 15:50:06 +0100

    setup Scaffolding

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">BOOKS</span>
+        </div>
+      </nav>
+</header>
+
